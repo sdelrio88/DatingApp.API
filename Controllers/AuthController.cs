cea2e30a8631b7d00ec2fce1c08d6783c3f03b3a
Steven Del Rio <delrio.steven@gmail.com>
2018-04-22 17:28:21 -0500

    Update Register

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -40,14 +40,13 @@ namespace DatingApp.API.Controllers
                 return BadRequest(ModelState);
 
 
-            var userToCreate = new User
-            {
-                Username = UserForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(UserForRegisterDto);
 
-            var createUser = await _repo.Register(userToCreate, UserForRegisterDto.Password);
+            var createdUser = await _repo.Register(userToCreate, UserForRegisterDto.Password);
 
-            return StatusCode(201); //temporary code
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+
+            return CreatedAtRoute("GetUser", new {controller = "Users", id = createdUser.Id}, userToReturn);
         }
 
         [HttpPost("login")]
